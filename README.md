e631a6c0627362b5f2f6a755590576ef255cc7b7
Kibum Moon <a072826@gmail.com>
2021-04-16 19:44:15 +0900

    update codes

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# The Mirror of Mind: Visualizing Mental Representations of Self Through Reverse Correlation
-
-→ [See the rearch paper](https://doi.org/10.3389/fpsyg.2020.01149) 
-
+# [The Mirror of Mind: Visualizing Mental Representations of Self Through Reverse Correlation](https://doi.org/10.3389/fpsyg.2020.01149)
 
 * R codes for generating self-CIs in the paper
--- generating-self-CIs.R
+++ generating-self-CIs.R
@@ -2,14 +2,16 @@ install.packages('rcicr')
 library(rcicr)
 library(tidyverse)
 
+# The rcicr packages does not allow use of tibble dataset 
 RCdata <- readxl::read_xlsx('The_Mirror_of_Mind.XLSX', sheet = 2) %>% 
-  data.frame() # The rcicr packages does not allow use of tibble dataset 
+  data.frame() 
 
-
+# The rcicr packages does not allow use of tibble dataset 
 RCdata <- readxl::read_xlsx('Table 1.XLSX', sheet = 2) %>% 
-    data.frame() # The rcicr packages does not allow use of tibble dataset 
+    data.frame() 
 
-RCdata # dataframe consists participants' ID, stimulus ID, response
+# A data-frame that contains participants' ID, stimulus ID, and responses
+RCdata 
 
 female_ci <- batchGenerateCI2IFC(RCdata %>% filter(ID == "ex_f"),  
                     by = "ID",
